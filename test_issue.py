0441ac291c09563778bbefd10bb922e542ccee41
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2023-10-04 00:03:53 +0200

    Reduced example further

--- test_issue.py
+++ test_issue.py
@@ -32,6 +32,7 @@ def test_add_zero_different_result():
     # prepare input
     img = torchvision.io.image.read_image('sky1024px.jpg')
     img = FT.convert_image_dtype(img, torch.float32)
+    img = img[:, :1, :3]  # take only a subset of the image
     img = img.unsqueeze(dim=0)
 
     # prepare input + zero
@@ -43,8 +44,8 @@ def test_add_zero_different_result():
     assert torch.equal(img, img_updated)
 
     # prepare model
-    conv1 = nn.Conv2d(in_channels=3, out_channels=129, kernel_size=3, padding=1)
-    conv2 = nn.Conv2d(in_channels=129, out_channels=4, kernel_size=1)
+    conv1 = nn.Conv2d(in_channels=3, out_channels=2, kernel_size=3, padding=1)
+    conv2 = nn.Conv2d(in_channels=2, out_channels=2, kernel_size=1)
     conv1.requires_grad_(False)
     conv2.requires_grad_(False)
 
@@ -56,11 +57,7 @@ def test_add_zero_different_result():
     y = conv1(img_updated)
     result2 = conv2(y)
 
-    # tensors after conv1 are equal
-    assert torch.allclose(x, y)
-    assert torch.equal(x, y)
-
     # ISSUE: the results are not equal but should be, since only zeros are added
-    print(torch.linalg.norm(result1 - result2))  # 1.6691e-06
+    print(torch.linalg.norm(result1 - result2))  # 2.9802e-08
     assert torch.allclose(result1, result2)
     assert torch.equal(result1, result2)
