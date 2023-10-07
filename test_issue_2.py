e5d49bb7079ab74f0d98edb3a957e1ab4ac3d7e2
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2023-10-07 17:43:29 +0200

    Minimized test case

--- test_issue_2.py
+++ test_issue_2.py
@@ -11,13 +11,13 @@ def test_add_zero_different_result_2():
     # prepare non-contiguous input
     img = torch.tensor(
         [
-            [[1.0, 1.0, 1.0]],
-            [[1.0, 1.0, 1.0]],
-            [[1.0, 1.0, 1.0]],
+            [[1.0, 2.0]],
+            [[3.0, 4.0]],
         ],
         dtype=torch.float32
-    ).permute([2, 1, 0])
+    )
 
+    img = img.permute([2, 1, 0])
     img = img.unsqueeze(dim=0)
 
     zeros = torch.zeros_like(img)
@@ -25,7 +25,7 @@ def test_add_zero_different_result_2():
     assert not img.is_contiguous()
     assert not img_updated.is_contiguous()
 
-    conv1 = nn.Conv2d(in_channels=3, out_channels=2, kernel_size=3, padding=1)
+    conv1 = nn.Conv2d(in_channels=2, out_channels=2, kernel_size=3, padding=1)
     conv1.requires_grad_(False)
 
     x = conv1(img)
@@ -33,8 +33,8 @@ def test_add_zero_different_result_2():
     assert x.is_contiguous()
     assert not y.is_contiguous()
 
-    print(torch.linalg.norm(x - y))  # 6.0069e-08
-    assert torch.allclose(x, y)
-    # assert torch.equal(x, y)
-
+    print(torch.linalg.norm(x - y))  # 5.960464478e-08
     print(torch.eq(x, y))
+
+    assert torch.allclose(x, y)
+    assert torch.equal(x, y)
